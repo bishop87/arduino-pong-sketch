a4deb159708fbb78e4ebccfee827e88fd2c1b4ba
Luca Vescovini <bishop87@gmail.com>
2024-04-29 11:35:08 +0200

    Update arduino-pong-sketch.ino
    minor code refactory

--- arduino-pong-sketch.ino
+++ arduino-pong-sketch.ino
@@ -24,7 +24,7 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 #define MAX_Y_SPEED 2
 //Define Variables
 
-int game = -1; //0: pong-1vs1; 1: pongCPU; 2: scketch
+int game = -1; //0: pong-1:1; 1: pong-cpu; 2: scketch
 int menuIndex = 0;
 const int menuItems = 3;
 String menu[menuItems] = {"PONG-1:1", "PONG-CPU", "SKETCH"};
@@ -41,10 +41,8 @@ int scoreA = 0;
 int scoreB = 0;
 
 
-//Setup
-void setup()
-{
-  Serial.begin(115200);
+void setup(){
+  //Serial.begin(115200);
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     //Serial.println(F("SSD1306 allocation failed"));
     for(;;);
@@ -55,11 +53,9 @@ void setup()
   display.clearDisplay();
 
   randomSeed(analogRead(0));
-  //int controlA = analogRead(CONTROL_A);
-  //menuIndex=controlA>500?1:2; 
 }
 
-void showMenu() {
+void showMenu(){
 
   int controlB = analogRead(CONTROL_B);  
   while (abs(controlB - analogRead(CONTROL_B)) < 10) { //controlB attiva la selezione corrente
@@ -71,18 +67,14 @@ void showMenu() {
     if(menuSelect != menuIndex) {
       menuIndex = menuSelect;
       drawMenu();
-    }
-  
-    //if(abs(controlB - analogRead(CONTROL_B)) < 5){
-    //  game=menuSelect;
-    // }
+    }  
   }  
 
   game=menuIndex;
   display.clearDisplay();
   
-  Serial.print("game: ");
-  Serial.println(game);
+  //Serial.print("game: ");
+  //Serial.println(game);
 }
 
 void drawMenu() {
@@ -121,25 +113,17 @@ void splash()
   display.display();
   delay(2000);
   soundStart();
-  /*
-  int controlA = analogRead(CONTROL_A);
-  int controlB = analogRead(CONTROL_B);
-  while (abs(controlA - analogRead(CONTROL_A) + controlB - analogRead(CONTROL_B)) < 10) {
-    // show as long as the total absolute change of
-    // both potmeters is smaler than 5
-  }*/
-  
 }
 
 void loop()
 {
   switch(game){
     case 0: //pong1vs1
-      calculateMovement();
+      calculateMovement(false);
       drawPong();
       break;
     case 1: //pongCPU
-      calculateMovementCPU();
+      calculateMovement(true);
       drawPong();
       break;  
     case 2: //sketch
@@ -151,77 +135,29 @@ void loop()
   }
 }
 
-void calculateMovement(){
-  int controlA = analogRead(CONTROL_A);
+//input vsCPU: se true abilita calcolo movimento paddle A
+void calculateMovement(bool vsCPU){
   int controlB = analogRead(CONTROL_B);
-  paddleLocationA = map(controlA, 0, 1023, 0, SCREEN_HEIGHT - PADDLE_HEIGHT);
+
+  if (vsCPU){    
+    const uint8_t half_paddle = PADDLE_HEIGHT >> 1;
+    if(paddleLocationA + half_paddle > ballY) {
+      paddleLocationA -= random(1,3);
+    }
+    if(paddleLocationA + half_paddle < ballY) {
+      paddleLocationA += random(1,3);
+    }
+  }else{
+    int controlA = analogRead(CONTROL_A);
+    paddleLocationA = map(controlA, 0, 1023, 0, SCREEN_HEIGHT - PADDLE_HEIGHT);
+  }
+
   paddleLocationB = map(controlB, 0, 1023, 0, SCREEN_HEIGHT - PADDLE_HEIGHT);
   int paddleSpeedA = paddleLocationA - lastPaddleLocationA;
   int paddleSpeedB = paddleLocationB - lastPaddleLocationB;
   ballX += ballSpeedX;
   ballY += ballSpeedY;
-  //bounce from top and bottom
-  if (ballY >= SCREEN_HEIGHT - BALL_SIZE || ballY <= 0) {
-    ballSpeedY *= -1;
-    soundBounce();
-  }
-  //bounce from paddle A
-  if (ballX >= PADDLE_PADDING && ballX <= PADDLE_PADDING+BALL_SIZE && ballSpeedX < 0) {
-    if (ballY > paddleLocationA - BALL_SIZE && ballY < paddleLocationA + PADDLE_HEIGHT) {
-      soundBounce();
-      ballSpeedX *= -1;
 
-      addEffect(paddleSpeedA);
-    }
-  }
-  //bounce from paddle B
-  if (ballX >= SCREEN_WIDTH-PADDLE_WIDTH-PADDLE_PADDING-BALL_SIZE && ballX <= SCREEN_WIDTH-PADDLE_PADDING-BALL_SIZE && ballSpeedX > 0) {
-    if (ballY > paddleLocationB - BALL_SIZE && ballY < paddleLocationB + PADDLE_HEIGHT) {
-      soundBounce();
-      ballSpeedX *= -1;
-
-      addEffect(paddleSpeedB);
-    }
-  }
-  //score points if ball hits wall behind paddle
-  if (ballX >= SCREEN_WIDTH - BALL_SIZE || ballX <= 0) {
-    if (ballSpeedX > 0) {
-      scoreA++;
-      ballX = SCREEN_WIDTH / 4;
-    }
-    if (ballSpeedX < 0) {
-      scoreB++;
-      ballX = SCREEN_WIDTH / 4 * 3;
-    }
-    soundPoint();  
-  }
-  //set last paddle locations
-  lastPaddleLocationA = paddleLocationA;
-  lastPaddleLocationB = paddleLocationB;  
-}
-
-
-void calculateMovementCPU(){
-  //int controlA = analogRead(CONTROL_A);
-  int controlB = analogRead(CONTROL_B);
-  
-  //paddleLocationA = map(controlA, 0, 1023, 0, SCREEN_HEIGHT - PADDLE_HEIGHT);
-  const uint8_t half_paddle = PADDLE_HEIGHT >> 1;
-  if(paddleLocationA + half_paddle > ballY) {
-    paddleLocationA -= random(1,3);
-  }
-  if(paddleLocationA + half_paddle < ballY) {
-    paddleLocationA += random(1,3);
-  }
-  
-  paddleLocationB = map(controlB, 0, 1023, 0, SCREEN_HEIGHT - PADDLE_HEIGHT);
-  
-  int paddleSpeedA = paddleLocationA - lastPaddleLocationA;
-  int paddleSpeedB = paddleLocationB - lastPaddleLocationB;
-
-  ballX += ballSpeedX;
-  ballY += ballSpeedY;
-  
   //bounce from top and bottom
   if (ballY >= SCREEN_HEIGHT - BALL_SIZE || ballY <= 0) {
     ballSpeedY *= -1;
@@ -291,8 +227,7 @@ void drawPong(){
   display.display();
 }
 
-void addEffect(int paddleSpeed)
-{
+void addEffect(int paddleSpeed){
   float oldBallSpeedY = ballSpeedY;
   //add effect to ball when paddle is moving while bouncing.
   //for every pixel of paddle movement, add or substact EFFECT_SPEED to ballspeed.
@@ -314,8 +249,7 @@ void addEffect(int paddleSpeed)
   if (ballSpeedY < -MAX_Y_SPEED) ballSpeedY = -MAX_Y_SPEED;
 }
 
-void soundStart()
-{
+void soundStart(){
   tone(BEEPER, 250);
   delay(100);
   tone(BEEPER, 500);
@@ -324,16 +258,13 @@ void soundStart()
   delay(100);
   noTone(BEEPER);
 }
-void soundBounce()
-{
+void soundBounce(){
   tone(BEEPER, 500, 50);
 }
-void soundPoint()
-{
+void soundPoint(){
   tone(BEEPER, 150, 150);
 }
-void centerPrint(char *text, int y, int size)
-{
+void centerPrint(char *text, int y, int size){
   display.setTextSize(size);
   display.setCursor(SCREEN_WIDTH/2 - ((strlen(text))*6*size)/2,y);
   display.print(text);
@@ -342,8 +273,8 @@ void centerPrint(char *text, int y, int size)
 void drawSchetch(){
   //static int phase = 0;
   static unsigned long next_serial_timestamp = 0;
-  int H = map(analogRead(A0), 0, 1023, 0, SCREEN_WIDTH - 1);
-  int V = map(analogRead(A1), 0, 1023, 0, SCREEN_HEIGHT - 1);
+  int H = map(analogRead(CONTROL_A), 0, 1023, 0, SCREEN_WIDTH - 1);
+  int V = map(analogRead(CONTROL_B), 0, 1023, 0, SCREEN_HEIGHT - 1);
 
   display.drawPixel(H, V, WHITE);
   display.display();
